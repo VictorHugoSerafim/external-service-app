f1fe41a202c6047398ffa664632149ebab0a49ed
Sato <satidelta@gmail.com>
2023-11-29 21:36:45 -0300

    somete desiine

--- frontend/app/screens/index.ts
+++ frontend/app/screens/index.ts
@@ -1,19 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
 export * from "./Login"
 export * from "./MinhasOrdens"
 export * from "./Home"
 export * from "./Historico"
 export * from "./OrdemServico"
-
-
-
-
-
-
-
 
 export * from "./Home"
 export * from "./ErrorScreen/ErrorBoundary"
